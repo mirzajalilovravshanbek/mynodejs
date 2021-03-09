fb952545186044855b8ded004d2d7e0f46c6d006
Ravshanbek <mirzajalilovravshanbek@gmail.com>
2021-03-09 17:27:06 +0500

    Initial commit

---
+++ mynpm/index.js
@@ -0,0 +1,8 @@
+let _ = require('underscore');
+
+let result = _.contains([2, 3, 8], 8);
+let rand = _.random(1, 200);
+let nowtime = _.now();
+let sam = _.sample([1, 2, 3, 4, 85, 96, 5, 6], 4);
+let shuf = _.shuffle([1, 2, 3, 4, 5, 6, 85, 96]);
+console.log(shuf);
